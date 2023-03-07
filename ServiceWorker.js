db50b70f7095d97a74fec01845b69e65b1328164
MultiversoDesignApps <126917954+MultiversoDesignApps@users.noreply.github.com>
2023-03-07 11:10:56 -0300

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Multiverso design-Caminho do conhecimento-1.0";
+const contentToCache = [
+    "Build/webcam3.loader.js",
+    "Build/webcam3.framework.js.unityweb",
+    "Build/webcam3.data.unityweb",
+    "Build/webcam3.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
